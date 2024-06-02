a1731d4db5bb0f82e89ca9e181320cab4137bc53
m-nicholasnu33 <mpnicholas21@gmail.com>
2024-06-02 06:01:16 -0500

    updating progress made through June 1

--- cd_account.py
+++ cd_account.py
@@ -1,6 +1,6 @@
 """Import the Account class from the Account.py file."""
 # ADD YOUR CODE HERE
-
+from Account import Account
 def create_cd_account(balance, interest_rate, months):
     """Creates a CD account, calculates interest earned, and updates the account balance.
 
@@ -16,6 +16,7 @@ def create_cd_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
+    savings_account_details = Account()
 
     # Calculate interest earned
     # ADD YOUR CODE HERE
