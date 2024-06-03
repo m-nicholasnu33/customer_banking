4a6e99faca78c166657453f21425482302b14964
m-nicholasnu33 <mpnicholas21@gmail.com>
2024-06-03 12:00:38 -0500

    Final commit of tested and functional cd_account.py file for submission

--- cd_account.py
+++ cd_account.py
@@ -19,27 +19,30 @@ def create_cd_account(balance, interest_rate, months):
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
 
-    my_cd = Account(cd_balance, float(0))
+    cd_instance = Account(balance, float(0))
 
     # Calculate interest earned
     # ADD YOUR CODE HERE
 
-    cd_interest_earned = cd_balance * (cd_interest/100 * cd_maturity/12)
+    cd_interest_earned = balance * (interest_rate/100 * months/12)
 
     # Update the CD account balance by adding the interest earned
     # ADD YOUR CODE HERE
 
-    updated_cd_balance =+ cd_interest_earned
+    updated_cd_balance = balance + cd_interest_earned
 
     # Pass the updated_balance to the set balance method using the instance of the CDAccount class.
     # ADD YOUR CODE HERE
 
-    my_cd.set_balance(updated_cd_balance)
+    cd_instance.set_balance(updated_cd_balance)
 
     # Pass the interest_earned to the set interest method using the instance of the CDAccount class.
     # ADD YOUR CODE HERE
 
-    my_cd.set_interest(cd_interest_earned)
+    cd_instance.set_interest(cd_interest_earned)
 
-    # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    # Return the updated balance and interest earned. 
+    # ADD YOUR CODE HERE
+    return updated_cd_balance, cd_interest_earned 
+
+
